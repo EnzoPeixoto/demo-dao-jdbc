e8df506179e7d8fa91b731844559e9b1c8991d38
EnzoPeixoto <enzopeixoto043@gmail.com>
2023-12-10 08:54:27 -0300

    Insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,5 +29,10 @@ public class Program {
         for (Seller obj: list){
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller seller1 = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(seller1);
+        System.out.println("Inserted! New id = " + seller1.getId());
     }
 }
